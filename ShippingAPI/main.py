f6cba13793fcd58d8b0f4e1fa649b22489be3982
Caio Olubuni <caiocsn@hotmail.com>
2023-07-20 16:45:46 -0300

    feat: adding ShippingAPI

--- ShippingAPI/main.py
+++ ShippingAPI/main.py
@@ -13,7 +13,7 @@ from db.db import SessionLocal
 
 app = FastAPI()
 
-@app.post("/payments/confirm/{order_id}")
+@app.post("/shipping/processing/{order_id}")
 def confirm_payment(order_id: int):
     db = SessionLocal()
     db_order = db.query(OrderDB).filter(OrderDB.id == order_id).first()
@@ -22,11 +22,11 @@ def confirm_payment(order_id: int):
         db.close()
         raise HTTPException(status_code=404, detail="Order not found")
 
-    if db_order.status != "created":
+    if db_order.status != "payed":
         db.close()
         raise HTTPException(status_code=400, detail="Invalid status change")
     
-    db_order.status = "payed"    
+    db_order.status = "processing"    
     db.commit()
     db.refresh(db_order)
     db.close()
@@ -35,8 +35,8 @@ def confirm_payment(order_id: int):
     order_dict["items"] = json.loads(db_order.items)
     return OrderRead(**order_dict)
 
-@app.post("/payments/cancel/{order_id}")
-def cancel_payment(order_id: int):
+@app.post("/shipping/sent/{order_id}")
+def confirm_payment(order_id: int):
     db = SessionLocal()
     db_order = db.query(OrderDB).filter(OrderDB.id == order_id).first()
     
@@ -44,11 +44,34 @@ def cancel_payment(order_id: int):
         db.close()
         raise HTTPException(status_code=404, detail="Order not found")
 
-    if db_order.status != "created":
+    if db_order.status not in ['processing', 'payed']:
         db.close()
         raise HTTPException(status_code=400, detail="Invalid status change")
     
-    db_order.status = "cancelled"    
+    db_order.status = "sent"    
+    db.commit()
+    db.refresh(db_order)
+    db.close()
+
+    order_dict = db_order.__dict__
+    order_dict["items"] = json.loads(db_order.items)
+    return OrderRead(**order_dict)
+
+
+@app.post("/shipping/delivered/{order_id}")
+def confirm_payment(order_id: int):
+    db = SessionLocal()
+    db_order = db.query(OrderDB).filter(OrderDB.id == order_id).first()
+    
+    if not db_order:
+        db.close()
+        raise HTTPException(status_code=404, detail="Order not found")
+
+    if db_order.status not in ['processing', 'payed', 'sent']:
+        db.close()
+        raise HTTPException(status_code=400, detail="Invalid status change")
+    
+    db_order.status = "delivered"    
     db.commit()
     db.refresh(db_order)
     db.close()
